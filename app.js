082838dfa5c54db499d1c334dce2c2adc36d2a40
Emmanuel Patricio <L192909399@cdguzman.tecnm.mx>
2025-09-23 22:59:09 -0600

    feat: Agregue la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo');
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor escribe un nombre válido.");
+        return;
+    }
+
+    amigos.push(nombre); 
+    input.value = "";   
+
+    actualizarLista(); 
+}
